cd078b2389e472aaadeb93b000b40a3666a96502
Raymond Sebastian Fajardo <63920552+undefined@users.noreply.github.com>
2021-11-25 20:47:26 +0000

    First commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import { LightningElement } from "lwc";
+
+export default class App extends LightningElement {
+  title = "Welcome to Factor-X";
+
+  showFeatures = true;
+
+  /**
+   * Getter for the features property
+   */
+  get features() {
+    return [
+      {
+        label: "Learn in the browser.",
+        icon: "utility:edit",
+      },
+      {
+        label: "View changes to code instantly with Live Compilation.",
+        icon: "utility:refresh",
+      },
+      {
+        label: "Style your components with SLDS.",
+        icon: "utility:brush",
+      },
+    ];
+  }
+}
